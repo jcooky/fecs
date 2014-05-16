e78af003c0eb9f94e6a9163e200eacf9a7b0fa06
chidea <sbw228@gmail.com>
2014-05-16 20:53:50 +0900

    fecs test add

---
+++ src/test/java/fecs/FecsTest.java
@@ -0,0 +1,10 @@
+package fecs;
+
+import junit.framework.TestCase;
+import org.python.util.PythonInterpreter;
+
+public class FecsTest extends TestCase {
+  public void testRun() throws Exception {
+
+  }
+}
