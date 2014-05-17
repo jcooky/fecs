2b064a45f4ce8a948d357e27f8db1af1ccbcaeaf
Byoungwoo Song (slate-PC) <sbw228@gmail.com>
2014-05-17 17:29:29 +0900

    PassengerMaker.makePassenger fix, static CabinController

--- src/main/scripts/__init__.py
+++ src/main/scripts/__init__.py
@@ -1,1 +1,9 @@
 __author__ = 'Byoungwoo'
+
+#global imports
+
+from fecs import CabinsController
+from fecs.model import CabinType, FloorType, Vector
+from java.lang.System import out
+
+out.println('jython global variables imported')
